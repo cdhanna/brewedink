0326782d4a06267e49795ba7e3c5c7644e0772e2
Chris <cdhannaphone@gmail.com>
2022-01-25 22:55:55 -0500

    adds shoop

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ var wfcMain = document.getElementById('wfc-main');
 var wfcElem = document.getElementById('wfc');
 
 
+var shoopMain = document.getElementById('shoop-main');
+var shoopElem = document.getElementById('shoop');
+
+
 function onLouieEnter(){
     louieElem.classList.add('active')
 }
@@ -21,8 +25,21 @@ function onWfcExit(){
     wfcElem.classList.remove('active')
 }
 
+function onShoopEnter(){
+    shoopElem.classList.add('active')
+}
+function onShoopExit(){
+    shoopElem.classList.remove('active')
+}
+
+
+setTimeout(function(){
 louieMain.addEventListener('mouseenter', onLouieEnter);
 louieElem.addEventListener('mouseleave', onLouieExit);
 
 wfcMain.addEventListener('mouseenter', onWfcEnter);
 wfcElem.addEventListener('mouseleave', onWfcExit);
+
+shoopMain.addEventListener('mouseenter', onShoopEnter);
+shoopElem.addEventListener('mouseleave', onShoopExit);
+}, 500);
